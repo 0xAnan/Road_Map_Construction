cb7d11e99474a1c550518a34d1b1184881f76084
AHMED HAANY <ahmed.el8areb12@gmail.com>
2024-05-04 15:19:31 +0300

    graoh add

--- Graph Classes/Graph.cpp
+++ Graph Classes/Graph.cpp
@@ -1,3 +1,31 @@
-//
-// Created by pc on 4/24/2024.
-//
+#include<iostream>
+#include<list>
+#include<utility>
+#include<unordered_map>
+using namespace std;
+class graph
+{
+public:
+    unordered_map<string, list<pair<string, int>>> mymap;
+    void addedge(string city1, string city2,int km)
+    {
+        mymap[city1].push_back(make_pair(city2, km));
+        mymap[city2].push_back(make_pair(city1, km));
+
+    }
+    void printadjcentlist()
+    {
+        for (auto c : mymap)
+        {
+            string city = c.first;
+            list<pair<string, int>> details=c.second;
+            for (auto dis : details)
+            {
+                cout << " destenation: " << dis.first << endl;
+                cout << " distance from " << c.first << " to " << dis.first;
+                cout << dis.second << " KM" << endl;
+                cout << "-------------------------------" << endl;
+            }
+        }
+    }
+};
